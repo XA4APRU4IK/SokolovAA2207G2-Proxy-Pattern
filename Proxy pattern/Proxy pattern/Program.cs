3a4588383495045defdc244fbade920c86d7cb18
XA4APRU4IK <cokolov123123@mail.ru>
2024-11-25 11:31:17 +0700

    Add files via upload

---
+++ Proxy pattern/Proxy pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Proxy_pattern;
+public interface ISubject
+{
+    string Request(string request);
+}
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ISubject proxy = new Proxy();
+
+        // Первый запрос, обработка через RealSubject
+        Console.WriteLine(proxy.Request("Запрос 1"));
+
+        // Второй запрос, результат будет кэширован
+        Console.WriteLine(proxy.Request("Запрос 1"));
+
+        // Ждем 7 секунд, чтобы кэш устарел
+        Thread.Sleep(7000);
+
+        // Запрос снова вызовет RealSubject, так как кэш устарел
+        Console.WriteLine(proxy.Request("Запрос 1"));
+    }
+    
+}
---
+++ Proxy pattern/Proxy pattern/Proxy.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy_pattern
+{
+    public class Proxy : ISubject
+    {
+        private readonly RealSubject _realSubject;
+        private readonly Dictionary<string, (string result, DateTime timestamp)> _cache;
+        private readonly TimeSpan _cacheDuration;
+
+        public Proxy()
+        {
+            _realSubject = new RealSubject();
+            _cache = new Dictionary<string, (string result, DateTime timestamp)>();
+            _cacheDuration = TimeSpan.FromSeconds(5);
+        }
+        public string Request(string request)
+        {
+            if (!HasAccess())
+            {
+                return "доступ запрещен";
+            }
+            if (_cache.TryGetValue(request, out var cacheEntry))
+            {
+                if (DateTime.Now - cacheEntry.timestamp > _cacheDuration)
+                {
+                    Console.WriteLine("Proxy: Возвращение результата из кэша.");
+                    return cacheEntry.result;
+                }
+                else
+                {
+                    _cache.Remove(request);
+                }
+            }
+            string result = _realSubject.Request(request);
+            _cache[request] = (result, DateTime.Now);
+            return result;
+        }
+        private bool HasAccess()
+        {
+            return true;
+        }
+        
+    }
+}
